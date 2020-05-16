5718f93c19938dccfeb6095a6a4d561664a32081
MeirambekBakabayev83 <M.Bakabayev@gmail.com>
2020-05-17 02:24:06 +0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'NurayWebProject';
+
+  constructor(updates:  SwUpdate) {    
+    updates.available.subscribe((event) => {
+      updates.activateUpdate().then(() => document.location.reload());    
+    });    
+  }
+
+  installDesktopApp(){
+    alert("SOON!!!");
+  }
 }
